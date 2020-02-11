ba7b867724ed9efdb493258b2cf29165cb922d5e
Ryan Branco <ryanbr23@gmail.com>
2020-02-11 10:39:45 -0800

    properly places pieces and gives black movement

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ const board = [
     null, 8, null, 9, null, 10, null, 11,
     null, null, null, null, null, null, null, null,
     null, null, null, null, null, null, null, null,
-    null, 12, null, 13, null, 14, null, 15,
-    16, null, 17, null, 18, null, 19, null,
-    null, 20, null, 21, null, 22, null, 23
+    12, null, 13, null, 14, null, 15, null,
+    null, 16, null, 17, null, 18, null, 19,
+    20, null, 21, null, 22, null, 23, null
 ]
 
 let turn = true; // true = reds turn, false = blacks turn
@@ -24,64 +24,113 @@ let turn = true; // true = reds turn, false = blacks turn
 
 /*---------- Cached Variables ----------*/
 
-const cells = document.querySelectorAll("td");
-const piece = document.querySelectorAll("p");
-const redTurnText = document.querySelector("#red-turn-text");
-const blackTurntext = document.querySelector("#black-turn-text");
-// const possibleMoves = {
-//     one: board.indexOf(pieceId + 7),
-//     two: board.indexOf(pieceId + 9),
-//     three: board.indexOf(pieceId - 7),
-//     four: board.indexOf(pieceId - 9)
-// }
-
-// plus or minus 14, 16, 18 are the available moves if you are taking away the oponents piece
-
-/*---------- Event Listeners ----------*/
-
-// Gives the pieces on "click" (for whoevers turn it is) and assigns the proper id to it
-if (turn) {
-    for (let i = 0; i < red.pieces.length; i++) {
-        piece[i].addEventListener("click", (event) => {
-            const pieceId = event.target.getAttribute("id");
-            event.target.style.border = "3px solid green";
-        })
-    }
-} else if (turn === false) {
-    for (let i = 0; i < black.pieces.length; i++) {
-        piece[red.pieces.length + i].addEventListener("click", (event) => {
-            const pieceId = event.target.getAttribute("id");
-            event.target.style.border = "3px solid green";
-
-        })
-    }
-}
-
-/*---------- Logic ----------*/
-
-// if you call parse(pieceId), it will give you the cells 
+// parses pieceId's and returns the index of that pieces place on the board
 let parse = function(pieceId) {
     let parsed = parseInt(pieceId);
     return board.indexOf(parsed);
 };
+const cells = document.querySelectorAll("td");
+const piece = document.querySelectorAll("p");
+const redTurnText = document.querySelector("#red-turn-text");
+const blackTurntext = document.querySelector("#black-turn-text");
 
-// give pieces on click {
-//      grab the id of that piece
-//      when pieces are on click change css outline to green
-//      highlight available locations by the isOccupied object and if its in range
-//      the next click assigns that id of the piece to the boards id that was clicked
-//      match the two id's of the board and the piece
-//      phisically move the piece on the screen to correct square
-//      change board object to have is occupied to true
-//      *** there will be a whole lot of logic here that i have no idea how it will work,
-//          but i have to some how check is a piece needs to get taken away ***
-//      invoke change player function
-//  }
+/*---------- Event Listeners ----------*/
 
+// Gives the pieces on "click" (for whoevers turn it is) and assigns the proper id to it
+function givePiecesClick() {
+    if (turn) {
+        for (let i = 0; i < red.pieces.length; i++) {
+            piece[i].addEventListener("click", (event) => {
+                const pieceId = event.target.getAttribute("id");
+                giveCellsClick(pieceId);
+            })
+        }
+    } else if (turn === false) {
+        for (let i = 0; i < black.pieces.length; i++) {
+            piece[red.pieces.length + i].addEventListener("click", (event) => {
+                const pieceId = event.target.getAttribute("id");
+                giveCellsClick(pieceId);
+            })
+        }
+    }
+}
+
+// Gives the cells on "click" (for whoevers turn it is)
+function giveCellsClick(pieceId) {
+    let parsedId = parse(pieceId);
+
+    // Helps normal red pieces move
+    if (turn && board[parsedId + 7] === null && cells[parsedId + 7].classList.contains("red") !== true) {
+        piece[pieceId].style.border = "3px solid green";
+        if (turn && board[parsedId + 9] === null && cells[parsedId + 9].classList.contains("red") !== true) {
+            cells[parsedId + 9].addEventListener("click", function giveClickToNine(event) {
+                event.target.innerHTML = `<p class="red-piece" id="${pieceId}"></p>`;
+                cells[parsedId].innerHTML = "";
+                piece[pieceId].border = "none";
+                deleteEventListeners(parsedId + 9);
+                changeData(pieceId, parsedId, parsedId + 9);
+            })
+        }
+        cells[parsedId + 7].addEventListener("click", function giveClickToSeven(event) {
+            event.target.innerHTML = `<p class="red-piece" id="${pieceId}"></p>`;
+            cells[parsedId].innerHTML = "";
+            piece[pieceId].border = "none";
+            deleteEventListeners(parsedId + 7);
+            changeData(pieceId, parsedId, parsedId + 7);
+        })
+    } else if (turn === false && board[parsedId - 7] === null && cells[parsedId - 7].classList.contains("red") !== true) {
+        piece[pieceId].style.border = "3px solid green";
+        if (turn === false && board[parsedId - 9] === null && cells[parsedId - 9].classList.contains("red") !== true) {
+            cells[parsedId - 9].addEventListener("click", function giveClickToNine(event) {
+                event.target.innerHTML = `<p class="black-piece" id="${pieceId}"></p>`;
+                cells[parsedId].innerHTML = "";
+                piece[pieceId].border = "none";
+                deleteEventListeners(parsedId - 9);
+                changeData(pieceId, parsedId, parsedId - 9);
+            })
+        }
+        cells[parsedId - 7].addEventListener("click", function giveClickToSeven(event) {
+            event.target.innerHTML = `<p class="black-piece" id="${pieceId}"></p>`;
+            cells[parsedId].innerHTML = "";
+            piece[pieceId].border = "none";
+            deleteEventListeners(parsedId - 7);
+            changeData(pieceId, parsedId, parsedId - 7);
+        })
+    } 
+}
+
+/*---------- Logic ----------*/
+
+// removes the event listeners that were just added
+
+
+/***** THERE IS A BUG IN HERE VVVVVVV FIX IT! *****/
+function deleteEventListeners(cellIdOne) {
+   
+}
+
+// Changes the board states data on the back end
+function changeData(pieceId, parsedId, modifiedParse) {
+    board[parsedId] = null;
+    board[modifiedParse] = parseInt(pieceId);
+    changePlayer();
+}
+
+// Switches players turn
 function changePlayer() {
+    if (turn) {
+        turn = false;
+        redTurnText.style.color = "lightGrey";
+        blackTurntext.style.color = "black";
+    } else if (turn === false) {
+        turn = true;
+        blackTurntext.style.color = "lightGrey";
+        redTurnText.style.color = "black"
+    }
     checkForWin();
 }
 
+// Checks every turn for a win
 function checkForWin() {
     if (black.pieces.length === 0) {
         blackTurntext.textContent = "";
@@ -90,4 +139,7 @@ function checkForWin() {
         redTurnText.textContent = "";
         blackTurntext.textContent = "BLACK WINS!"
     }
+    givePiecesClick();
 }
+
+givePiecesClick();
